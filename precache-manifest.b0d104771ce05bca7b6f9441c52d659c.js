e3f5f735dca815e42a34a8d5831f44db6598cbf4
minsk-sku <gdrx100@gmail.com>
2020-08-19 14:57:43 +0900

    Updates

---
+++ precache-manifest.b0d104771ce05bca7b6f9441c52d659c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c946fbac4400e41e86d7a2d370e99e",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "e7bce2c796debe162a0a",
+    "url": "/react_test/static/css/main.34b12bab.chunk.css"
+  },
+  {
+    "revision": "6db0110c92c4c0bfe143",
+    "url": "/react_test/static/js/2.98f8db21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_test/static/js/2.98f8db21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7bce2c796debe162a0a",
+    "url": "/react_test/static/js/main.b58d08c5.chunk.js"
+  },
+  {
+    "revision": "0acfb237109b04b55a98",
+    "url": "/react_test/static/js/runtime-main.8b15b1c3.js"
+  }
+]);
